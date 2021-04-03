c02c2e065920ba295c49e1cb19ad774ec80b3c6a
Marek Czyż <Marek.Czyz@dreamlab.pl>
2021-04-03 11:58:17 +0200

    Cache added to visited urls

--- src/Slider/index.js
+++ src/Slider/index.js
@@ -4,7 +4,7 @@ import "./slider.css";
 import axios from "axios";
 import PropTypes from "prop-types";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Button } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 const token = Buffer.from(
   `${process.env.USER}:${process.env.PASSWORD}`,
   "utf8"
@@ -19,21 +19,33 @@ const basicAuth = {
 const Slider = () => {
   const [url, setUrl] = useState(`https://api.github.com/users/gaearon`);
   const [data, setData] = useState({});
+  const [cachedData, setCachedData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const nicks = ["gaearon", "acdlite", "yyx990803", "unclebob", "martinfowler"];
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-
-      const result = await axios(url, {}, { headers: basicAuth });
-      setData(result.data);
-      setIsLoading(false);
+      if (cachedData[url]) {
+        setError(false);
+        setData(cachedData[url]);
+        setIsLoading(false);
+        return;
+      }
+      try {
+        const result = await axios(url, {}, { headers: basicAuth });
+        setError(false);
+        setData(result.data);
+        setCachedData({ ...cachedData, [url]: result.data });
+        setIsLoading(false);
+      } catch (e) {
+        setError(e);
+      }
     };
 
     fetchData();
   }, [url]);
-
   return (
     <Carousel
       autoPlay={false}
@@ -50,15 +62,24 @@ const Slider = () => {
       }}
     >
       {nicks.map((nick, i) => (
-        <Item key={i} nick={nick} data={data} isLoading={isLoading} />
+        <Item
+          key={i}
+          nick={nick}
+          data={data}
+          isLoading={isLoading}
+          error={error}
+        />
       ))}
     </Carousel>
   );
 };
 
 const Item = (props) => {
+  if (props.error) {
+    return <div>{`Error`}</div>;
+  }
   return props.isLoading ? (
-    "LOADING..."
+    <div>{"LOADING..."}</div>
   ) : (
     <Paper>
       <h2>{props.nick}</h2>
@@ -73,6 +94,7 @@ Item.propTypes = {
     avatar_url: PropTypes.string,
   }),
   isLoading: PropTypes.bool,
+  error: PropTypes.object,
 };
 
 export default Slider;
